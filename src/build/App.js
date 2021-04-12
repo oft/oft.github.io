1f29c2f76946113502fb1ee05d4e1d20432ed90b
xczq <290353142@qq.com>
2021-04-12 14:03:41 +0800

    hello

--- src/build/App.js
+++
@@ -1,8 +0,0 @@
-import React from "./_snowpack/pkg/react.js";
-export class App extends React.Component {
-  render() {
-    return /* @__PURE__ */ React.createElement(React.Fragment, null, /* @__PURE__ */ React.createElement("div", {
-      className: "flex"
-    }, /* @__PURE__ */ React.createElement("div", null, "Header"), /* @__PURE__ */ React.createElement("div", null, "Header")));
-  }
-}
